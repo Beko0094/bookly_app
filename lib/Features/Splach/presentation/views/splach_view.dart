0f601974d38e2fcc450cfbaac7df307e8c71a056
abubakeralzain <bekozain94@gmail.com>
2025-08-05 21:17:13 +0200

    first comit

---
+++ lib/Features/Splach/presentation/views/splach_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/Splach/presentation/views/widgets/splach_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
