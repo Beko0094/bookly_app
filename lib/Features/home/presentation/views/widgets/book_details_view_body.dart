bf6a4d505a3dbb99a1f9aba971050a9ee09d2d32
abubakeralzain <bekozain94@gmail.com>
2025-08-08 12:05:55 +0200

    add titele and subtitle and rating to the book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/Core/Utilits/styles.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_appbar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -11,12 +13,30 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const CustomeBookDetailsAppbar(),
+          const SizedBox(height: 20),
           Padding(
-            padding:
-                EdgeInsets.symmetric(horizontal: width * .16, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: width * .16),
             child: const CustombookImage(),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          SizedBox(height: 5),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w500),
+            ),
+          ),
+          SizedBox(height: 16),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -3,26 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 35),
       child: Row(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: mainAxisAlignment,
         children: [
           Icon(
             FontAwesomeIcons.solidStar,
             color: Color(0xffFFDD4F),
           ),
-          const SizedBox(width: 8),
+          const SizedBox(width: 12),
           Text('4.8',
               style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold)),
-          const SizedBox(width: 6),
-          Text('(2390)',
-              style: Styles.textStyle14.copyWith(
-                  fontWeight: FontWeight.w400, color: Color(0xff707070))),
+          const SizedBox(width: 10),
+          Opacity(
+            opacity: .5,
+            child: Text('(2390)',
+                style:
+                    Styles.textStyle14.copyWith(fontWeight: FontWeight.w400)),
+          ),
         ],
       ),
     );
