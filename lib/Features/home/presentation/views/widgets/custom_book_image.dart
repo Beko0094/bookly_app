92a7c9dcd60c70f90ecbf60eaf8f6e218f6da905
abubakeralzain <bekozain94@gmail.com>
2025-08-11 00:21:20 +0200

    add cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Core/Utilits/app_router.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,12 +16,23 @@ class CustombookImage extends StatelessWidget {
       child: AspectRatio(
         aspectRatio: 2.5 / 4,
         child: Container(
-          decoration: BoxDecoration(
-              color: Colors.green,
+            decoration: BoxDecoration(
+              border: Border.all(
+                  color: Colors.blue, width: 0.5), // تحديد اللون والسمك
+              borderRadius: BorderRadius.circular(16), // اختياري: زوايا دائرية
+            ),
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                  fit: BoxFit.fill, image: NetworkImage(imageUrl))),
-        ),
+              child: CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: imageUrl,
+                errorWidget: (context, url, error) => Icon(
+                  Icons.abc_outlined,
+                  color: Colors.blue,
+                  size: 50,
+                ),
+              ),
+            )),
       ),
     );
   }
